912c34b2fbe3e854485892fe7cdeded2848209fe
eabadir <sam@aspirevc.com>
2026-04-17 13:54:58 -0400

    style(InformationTheory/Hamming): restore accidentally removed blank line

--- Mathlib/InformationTheory/Hamming.lean
+++ Mathlib/InformationTheory/Hamming.lean
@@ -261,6 +261,7 @@ instance (α) [Semiring α] (β : ι → Type*) [∀ i, AddCommMonoid (β i)] [�
 
 /-! API to/from the type synonym. -/
 
+
 /-- `Hamming.toHamming` is the identity function to the `Hamming` of a type. -/
 @[match_pattern]
 def toHamming : (∀ i, β i) ≃ Hamming β :=
